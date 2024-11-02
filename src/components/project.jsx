1fa8409a02bd9cd4b538bbfc1b3f789b43d290c2
Jeyaparkash <145859559+jeyaprakash1234@users.noreply.github.com>
2024-11-02 13:19:21 +0530

    Update project.jsx

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -21,7 +21,12 @@ const project = () => {
         <h3 className="project-title">Laundry Website</h3>
      <a href="https://frontend-project-eight-pi.vercel.app/"><p className="project-year">View Project</p></a>
       </div>
+illustration-design
 
+              <div className="project-image illustration-design  "></div>
+        <h3 className="project-title">Food Delivery </h3>
+     <a href="https://food-delivery-two-neon.vercel.app/"><p className="project-year">View Project</p></a>
+      </div>
       
     </div>
   </div>
